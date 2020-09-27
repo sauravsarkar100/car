0071da3b9f537d0163bc294340b563e3358ff1ff
sauravsarkar100 <70365386+sauravsarkar100@users.noreply.github.com>
2020-09-27 16:23:31 +0600

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,18 @@
+function show_papsfst(){
+    document.getElementById('papsi-img').src="images/pexels-mike-3729464-removebg-preview.png";
+    document.getElementsByClassName('main')[0].style.backgroundColor='rgb(155, 39, 39)';
+}
+
+function show_papssec()
+{
+    document.getElementById('papsi-img').src="images/pexels-mike-810357-removebg-preview.png"
+    document.getElementsByClassName('main')[0].style.backgroundColor='rgb(43, 78, 78)';
+}
+
+function show_papstrd()
+{
+    document.getElementById('papsi-img').src="images/pexels-mike-241316-removebg-preview.png";
+    document.getElementsByClassName('main')[0].style.backgroundColor='rgb(79, 90, 90)';
+}
+
+
